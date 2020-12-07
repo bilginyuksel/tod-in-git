e399943c7699b2d7174d60d40a52765170150f74
bilginyuksel <bilgin.yuksel96@gmail.com>
2020-12-07 23:54:54 +0300

    tod improvements declared. using pickle and seperate files by author names

--- experiment.py
+++ experiment.py
@@ -1,6 +1,8 @@
 import os
 import re
+import pickle
 from datetime import datetime
+
 
 
 def getCommandResult(command):
@@ -25,28 +27,57 @@ Description $TEMPLATE$: asıjdaıjdsa
 $branch$: dev/init
 
 """
+class User:
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
 
+def getUserInformation():
+    username = getCommandResult("git config --local user.name")
+    email = getCommandResult("git config --local user.email")
+    if len(username) == 0:
+        username = getCommandResult("git config --global user.name")
+        email = getCommandResult("git config --local user.email")
+
+    return User(username.replace("\n", ""), email.replace("\n", ""))
+
+
+currentUser = getUserInformation()
+todos = {}
 class Todo:
-    def __init__(self, branch, note):
+    def __init__(self, branch, title, description=""):
         self.branch = branch
-        self.note = note
+        self.title = title
+        self.description = description
         self.createdTime = datetime.now()
         self.author = currentUser
+        self.importance = 2
+
+    def get_filename(self):
+        return str(self.branch + "_" + self.author).lower()
 
     def __str__(self):
         return "branch: %s, note: %s, author: %s, createdTime: %s," % (self.branch, self.note, self.author.username, self.createdTime)
 
+
+def save(o):
+    outputFile = open(".tod/demo", "wb")
+    pickle.dump(o, outputFile)
+    outputFile.close()
+
+
+def read():
+    readFile = open(".tod/demo", "rb")
+    content = pickle.load(readFile)
+    print(content)
+    return content
 
 class Branch:
     def __init__(self):
         pass
 
 
-class User:
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
 
 
 def createTodBranchesIfNotExists(branches):
@@ -81,31 +112,27 @@ def init():
     # otherwise create the file structure
     os.mkdir(".tod")
     # sync with current git.
-    cleanedOutput = branchResult.replace(
-        "*", "").replace("\t", "").replace(" ", "")
-    branches = [branch for branch in cleanedOutput.split(
-        "\n") if len(branch) > 0]
+    cleanedOutput = branchResult.replace("*", "").replace("\t", "").replace(" ", "")
+    branches = [branch for branch in cleanedOutput.split("\n") if len(branch) > 0]
     createTodBranchesIfNotExists(branches)
 
 # Get user informations from git.
 
 
-def getUserInformation():
-    username = getCommandResult("git config --local user.name")
-    email = getCommandResult("git config --local user.email")
-    if len(username) == 0:
-        username = getCommandResult("git config --global user.name")
-        email = getCommandResult("git config --local user.email")
-
-    return User(username.replace("\n", ""), email.replace("\n", ""))
 
 
 def getCurrentBranch():
     return getCommandResult("git branch")
 
 
-currentUser = getUserInformation()
-note1 = Todo(1, "My new note!")
-print(note1)
+# currentUser = getUserInformation()
+# note1 = Todo(1, "My new note!")
+# print(note1)
 
-init()
+# init()
+todos["131413"] = Todo("branch", "My first note title", "My first description")
+todos["0000"] =  Todo("boranch", "My second note title", "My second description")
+todos["123000"] = Todo("branch", "My third note title", "My third description")
+
+save(todos)
+content  = read()
