0407f6016c42d24214f3b7ba176c46e15e2f2f9b
bilginyuksel <bilgin.yuksel96@gmail.com>
2021-02-16 19:36:39 +0300

    refactor! experimental writing added

--- util/file_test.go
+++ util/file_test.go
@@ -1,11 +1,22 @@
 package util
 
 import (
-	"io/ioutil"
 	"testing"
+    "os"
+    "log"
 )
 
 func TestWriteByteFile_ByteContent(t *testing.T) {
 	// ioutil.ReadFile("")
-	ioutil.WriteFile("newfile", []byte("my data"), 0755)
+
+    f, err := os.OpenFile("newfile", os.O_APPEND | os.O_CREATE | os.O_WRONLY, 0644)
+    if err != nil {
+        log.Println(err)
+    }
+
+    defer f.Close()
+    _, err = f.Write([]byte("my byte string asdsa"))
+    if err != nil {
+        log.Println(err)
+    }
 }
